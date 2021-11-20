55ef4dba23facd1bea1f69ca100cc1b5b415cd30
Arinjoy_Programmer <90265138+ArinjoyProgrammer@users.noreply.github.com>
2021-11-20 12:54:02 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="packagearinjoyn",
-version="0.3",
+version="0.5",
 description="This is Arinjoy's Python Package!",
 long_description="This is a very bery long description!",
 author="Arinjoy",
